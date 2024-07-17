7031f221b9186003e0cfff0f7aaa1f2395169f09
Rutwik Shete <rutwik.shete@gmail.com>
2024-07-18 02:59:17 +0530

    Removing Projects And Adding Coming Soon

--- public/projects/refy/content.ts
+++ public/projects/refy/content.ts
@@ -1,16 +1,16 @@
 import { Content } from '@/content/projects';
 
 export const content: Content = {
-  title: 'Refy: AI powered mortgage broker',
+  title: 'Coming Soon',
   featuredImage: `overview.png`,
-  description: `Your Refy broker will be your best guide and advisor during the process of buying or refinancing your property. Leverages AI tools to calculate the lowest rates and get the financing that suits you best.`,
-  seo: `I created a web application that leverages dynamic server side rendering and serverless API with Next.js`,
+  description: `Porfolio In Development Coming Soon`,
+  seo: `Coming Soon Portfolio Of Rutwik Shete`,
   details: {
-    type: `Financial services`,
-    stack: `Typescript React Next.js Apollo_GraphQL Node.js PostgreSQL Vercel Route_53 AWS_Lambda AWS_Elastic_Beanstalk Cloudinary Airtable Storyblok`,
+    type: `Adding Soon`,
+    stack: `Coming Soon`,
     code: ` `,
-    live: `http://refy.cl/`,
+    live: `https://google.com`,
   },
-  keywords: ['nextjs', 'typescript', 'graphql', 'apollo', 'react'],
-  role: `FULL STACK DEVELOPER / TECH LEAD`,
+  keywords: ['Coming','Soon'],
+  role: `AI Engineer`,
 };
